2b96c976833ceaaaf7a975c8f387c06d68c66d56
JuliePruvost <julie.de.pruvost@gmail.com>
2022-09-06 08:58:49 +0200

    responsive navbar

--- src/components/navigation/NavigationBar.tsx
+++ src/components/navigation/NavigationBar.tsx
@@ -3,36 +3,61 @@ import { NavLink } from "react-router-dom";
 import { mainRoute, routeDescriptions } from "./Routes";
 import {ReactComponent as MainNavItemSvg} from './components/MainNavItem.svg';
 import './NavigationBar.scss';
+import { Container, Navbar } from "react-bootstrap";
+import { useState } from "react";
 
-const navigationItems = routeDescriptions.map((routeDescription, index) => {
-    if (!routeDescription.path) {
-        return <></>;
-    }
-
-    if (routeDescription === mainRoute) {
-        return (
-            <Nav.Item key={index}>
-                <Nav.Link as={NavLink} to={routeDescription.path} className="main-nav-item">
-                    <MainNavItemSvg />
-                </Nav.Link>
-            </Nav.Item>
-        )
-    }
-
+const MainNavigationItem = () => {
     return (
-        <Nav.Item key={index} className="common-nav-item">
-            <Nav.Link as={NavLink} to={routeDescription.path}>{routeDescription.label}</Nav.Link>
+        <Nav.Item key={0}>
+            <Nav.Link as={NavLink} to={routeDescriptions[0].path!} className="main-nav-item">
+                <MainNavItemSvg />
+            </Nav.Link>
         </Nav.Item>
     );
-});
+}
 
 export default function NavigationBar() {
+    const [expanded, setExpanded] = useState(false);
+
+    const onToggleClick = () => {
+        setExpanded(expanded ? false : true);
+    };
+
+    const onItemClick = () => {
+        setExpanded(false);
+    };
+
+    const navigationItems = routeDescriptions.map((routeDescription, index) => {
+        if (!routeDescription.path) {
+            return <></>;
+        }
+        if (routeDescription === mainRoute) {
+            return <></>;
+        }
+    
+        return (
+            <Nav.Item key={index} className="common-nav-item">
+                <Nav.Link as={NavLink} onClick={onItemClick} to={routeDescription.path}>{routeDescription.label}</Nav.Link>
+            </Nav.Item>
+        );
+    });
+
     return (
-        <Nav
-            activeKey="/"
-            // onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
-        >
-            {navigationItems}
-        </Nav>
+        <Container>
+        <Navbar expand="sm" expanded={expanded}>
+            <Container>
+                <Navbar.Brand href="#home"><MainNavigationItem /></Navbar.Brand>
+                <Navbar.Toggle onClick={onToggleClick} aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="me-auto"
+                        activeKey="/"
+                        // onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
+                    >
+                        {navigationItems}
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+        </Container>
     );
 }
